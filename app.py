af514719d9b007254c42667fe6d14d4c0042829a
Adam <adamlevitz@gmail.com>
2020-01-29 15:55:34 -0800

    WIP new user default img

--- app.py
+++ app.py
@@ -18,3 +18,27 @@ def root():
     users = Users.query.all()
 
     return render_template('list.html', users=users)
+
+@app.route('/users/new')
+def new_user_form():
+
+    return render_template('new_user.html')
+
+@app.route('/users/new', methods=['POST'])
+def add_new_user_to_db():
+
+    new_first_name = request.form['new_first_name']
+    new_last_name = request.form['new_last_name']
+    new_user_image = request.form['new_user_image']
+
+    user = Users(first_name=new_first_name, last_name=new_last_name, image_url=new_user_image)
+
+    db.session.add(user)
+    db.session.commit()
+
+
+    print(new_first_name,new_last_name, new_user_image)
+
+    return redirect('/')
+
+
